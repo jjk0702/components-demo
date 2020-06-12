3982e836063d567698a29cd8bf14cef9d6d2bdd7
jjk0702 <50093825+jjk0702@users.noreply.github.com>
2020-06-12 13:26:00 +0800

    新建一个spring boot + mybatisplus项目

---
+++ test-mybatisplus/src/main/java/com/jjk/testmybatisplus/TestMybatisplusApplication.java
@@ -0,0 +1,13 @@
+package com.jjk.testmybatisplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMybatisplusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMybatisplusApplication.class, args);
+    }
+
+}
